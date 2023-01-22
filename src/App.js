44664fbaba378d1fcf66107014e15539b380866b
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2023-01-22 14:46:48 -0500

    test: render, function, component render, elements, search types, variants, fire events & user events

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import sum from "./math";
+import { useState, useEffect } from "react";
+
+const getUser = () => {
+  return Promise.resolve({ id: '1', name: 'Daniel' });
+};
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [search, setSearch] = useState('');
+  const title = "Hello Earthlings 🛸"
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    };
+
+    loadUser();
+  }, [])
+  
+
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="app-contents">
+          <div className="sum">
+            <a
+              className="App-link"
+              href="https://github.com/DanielYankiver"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Sum Function Output: {sum(5, 4)}
+            </a>
+          </div>
+          <div>{title}</div>
+        </div>
+        <div className="search">
+          {user ? <p>Signed in as {user.name}</p> : null}
+          <Search value={search} onChange={handleChange}>
+            Search:
+          </Search>
+          <div>Searches for {search ? search : '...'}</div>
+        </div>
+    </div>
+  );
+}
+
+function Search({ value, onChange, children }) {
+  return (
+    <div>
+      <label htmlFor="search">{children}</label>
+      <input
+        id="search"
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,100 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import sum from './math'
 
-test('renders learn react link', () => {
+// test and it can be named the same thing 
+test('renders app & sum output with link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sum Function Output:/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+// describe-block is the test suite
 describe('true is truthy and false is falsy', () => {
   it('true is truthy', () => {
     expect(true).toBe(true);
   });
 
+  // the it-block (which also can be named test instead of it) is the test case
   it('false is falsy', () => {
     expect(false).toBe(false);
   });
 });
+
+// got sum function from math.js file and tested it 
+describe('sum', () => {
+  it('sums up two values', () => {
+    expect(sum(2, 4)).toBe(6);
+  });
+});
+
+describe('App', () => {
+  it('renders App component', async () => {
+    render(<App />)
+ 
+    // renders HTML --> RTL sees output human sees 
+    // screen.debug();
+
+    // implicit assertion
+    // because getByText would throw error
+    // if element wouldn't be there
+    screen.getByText('Search:');
+
+    // explicit assertion
+    // recommended
+    expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    // The neat thing about getByRole: it shows all the selectable roles if you provide a role that isn't available in the rendered component's HTML:
+    // screen.getByRole('')
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+
+    // Every time you are asserting that an element isn't there, use queryBy. Otherwise default to getBy.
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    // The findBy search variant is used for asynchronous elements which will be there eventually.
+
+    // After its initial render, we assert that the "Signed in as" text is not there by using the queryBy instead of the getBy search variant.
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+    // screen.debug();
+
+    // Then we await the new element to be found, and it will be found eventually when the promise resolves and the component re-renders again.
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+    // screen.debug();
+
+    // FIRE EVENT:
+
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/);;
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+    
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+
+    // Can do this instead of above:
+    waitFor(() =>
+      expect(
+        screen.getByText(/Searches for JavaScript/)
+      ).toBeInTheDocument()
+    );
+
+    // USER EVENT
+
+    // wait for the user to resolve
+    // await screen.findByText(/Signed in as/);
+
+    // expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    // await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+
+    // expect(
+    //   screen.getByText(/Searches for JavaScript/)
+    // ).toBeInTheDocument();
+
+
+  })
+})
